39577e849f01cad91e9b809ea62ffad3290418ed
Sufian Javed <sufian_jawed@hotmail.com>
2021-03-21 04:09:08 +0500

    Corrected glue

--- src/test/java/runner/RunSerenityTest.java
+++ src/test/java/runner/RunSerenityTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
-        glue = {"helpers","stepdefs"},
+        glue = "stepdefs",
         features = "src/test/resources/features")
 public class RunSerenityTest {
 }
